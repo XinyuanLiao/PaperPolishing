932936d7abd1b0e4a5d0cfbdd53a2b2852bfd980
XinyuanLiao <122154029+XinyuanLiao@users.noreply.github.com>
2023-06-01 19:10:21 +0800

    Add files via upload

---
+++ PaperPolishing/ChatGPT.py
@@ -0,0 +1,32 @@
+import os
+import openai
+
+os.environ["HTTP_PROXY"] = "127.0.0.1:1082"
+os.environ["HTTPS_PROXY"] = "127.0.0.1:1082"
+
+openai.api_key = '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'
+
+
+def response(question):
+    question = translate(question)
+    q = "以下是一篇学术论文的一段话，打磨文字以符合学术风格，提高拼写，语法，清晰度，简洁性和整体可读性。必要时重写整个句子。此外，在后面直接列出修改后的中文翻译：" + question
+    rsp = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "一个计算机方向的论文专家"},
+            {"role": "user", "content": q}
+        ]
+    )
+    return rsp.get("choices")[0]["message"]["content"]
+
+
+def translate(chinese):
+    q = "如果下列语句是中文，将其翻译成英文输出，如果是英文则重复这句话即可：" + chinese
+    rsp = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "中文到英文的翻译学家"},
+            {"role": "user", "content": q}
+        ]
+    )
+    return rsp.get("choices")[0]["message"]["content"]
---
+++ PaperPolishing/GUI.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+import ChatGPT
+
+sg.theme('default1')  # 设置主题
+font = ("Times New Roman", 16)
+font1 = ("Helvetica", 20)
+layout = [[sg.Text(text='中文或需要润色的英文：', size=(52, 1), font=font1)],
+          [sg.Multiline(key="-IN-", size=(75, 10), font=font)],
+          [sg.Text(text='润色后的英文及其中文翻译：', size=(50, 1), font=font1)],
+          [sg.Multiline(key="-OUT-", size=(75, 10), font=font)],
+          [sg.Text(text='', size=(30, 1)),
+           sg.Button("润色", size=(15, 1), button_color='#a5a7ab', font=font),
+           sg.Button("清除", size=(15, 1), button_color='#a5a7ab', font=font),
+           sg.Button("退出", size=(15, 1), button_color='#a5a7ab', font=font)]
+          ]
+window = sg.Window("论文翻译并润色", layout)  # 设置窗口名称，窗口布局，以及图标
+
+while True:
+    event, values = window.read()
+    if event in (None, "退出"):  # 点击“X”或者“退出”按钮时才退出
+        break
+    if event == "润色":
+        tar = ChatGPT.response(values["-IN-"])
+        window["-OUT-"].Update(tar)
+    if event == "清除":
+        window["-IN-"].Update("")
+        window["-OUT-"].Update("")
+window.close()
