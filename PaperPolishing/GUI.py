ee256f0d024b89f26bdbd64ba94be60a617b7a37
XinyuanLiao <122154029+XinyuanLiao@users.noreply.github.com>
2023-06-04 19:16:08 +0800

    Update GUI.py

--- PaperPolishing/GUI.py
+++ PaperPolishing/GUI.py
@@ -7,7 +7,7 @@ font1 = ("Helvetica", 20)
 layout = [[sg.Text(text='中文或需要润色的英文：', size=(52, 1), font=font1)],
           [sg.Multiline(key="-IN-", size=(75, 10), font=font)],
           [sg.Text(text='润色后的英文及其中文翻译：', size=(50, 1), font=font1)],
-          [sg.Multiline(key="-OUT-", size=(75, 10), font=font)],
+          [sg.Multiline(key="-OUT-", size=(75, 10), font=font, default_text=ChatGPT.check_connection())],
           [sg.Text(text='', size=(10, 1)),
            sg.Button("翻译", size=(15, 1), button_color='#a5a7ab', font=font),
            sg.Button("润色", size=(15, 1), button_color='#a5a7ab', font=font),
@@ -15,7 +15,7 @@ layout = [[sg.Text(text='中文或需要润色的英文：', size=(52, 1), font=
            sg.Button("退出", size=(15, 1), button_color='#a5a7ab', font=font)]
           ]
 window = sg.Window("PaperPolishing", layout)  # 设置窗口名称，窗口布局，以及图标
-
+# print(ChatGPT.check_connection())
 while True:
     event, values = window.read()
     if event in (None, "退出"):  # 点击“X”或者“退出”按钮时才退出
